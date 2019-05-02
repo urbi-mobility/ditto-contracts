a0bec32b442c153015428194d930162390ec512e
vrde <agranzot@gmail.com>
2019-05-02 12:26:26 +0200

    Add first test

--- test/test_registry.js
+++ test/test_registry.js
@@ -1,8 +1,20 @@
+const assert = require("assert");
 const Registry = artifacts.require("Registry");
 
 contract("Registry", async accounts => {
+  // Simulate different actors.
+  // - `admin`: the super organization that adds or removes certification
+  // authorities.
+  // - `caUrbi`: Urbi as a certification authority.
+  // - `ca[1..n]`: other certification authorities.
 
-  it("when created has only one Whitelist Admin", async () => {});
+  const [admin, caUrbi, ca1, ca2] = accounts;
+
+  it("when created has only one Whitelist Admin", async () => {
+    const r = await Registry.deployed();
+    assert.equal(await r.isWhitelistAdmin.call(admin), true);
+    assert.equal(await r.isWhitelistAdmin.call(caUrbi), false);
+  });
 
   it("allows to add a Certification Authority", async () => {});
 
